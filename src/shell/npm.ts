c036ff7a048fb6aee3461cde6e2403b4bcc40332
tkiddo <justforgit@163.com>
2022-05-27 11:00:28 +0800

    feat: npm registry get and set

---
+++ src/shell/npm.ts
@@ -0,0 +1,7 @@
+export const registries = {
+  npm: "https://registry.npmjs.org/",
+  taobao: "https://registry.npm.taobao.org/",
+};
+
+export const getRegistry = "npm config get registry";
+export const setRegstry = "npm config set registry [registry url]";
