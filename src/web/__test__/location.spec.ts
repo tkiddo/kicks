4e161b78d4ff7ee9f36bd70caab680ee533336a3
tkiddo <justforgit@163.com>
2022-06-15 17:18:20 +0800

    feat: location query object

--- src/web/__test__/location.spec.ts
+++ src/web/__test__/location.spec.ts
@@ -1,7 +1,12 @@
-import { getQueryString } from "../location";
+import { getQueryString, getQueryObject } from "../location";
 
 test("getQueryString function", () => {
   const search = "?name=John&age=30";
   expect(getQueryString(search, "name")).toBe("John");
   expect(getQueryString(search, "age")).toBe("30");
+});
+
+test("getQueryObject function", () => {
+  const search = "?name=John&age=30";
+  expect(getQueryObject(search)).toEqual({ name: "John", age: "30" });
 });
--- src/web/location.ts
+++ src/web/location.ts
@@ -3,3 +3,13 @@ export const getQueryString = (search: string, paramName: string) => {
   const r = search.substring(1).match(reg);
   if (r != null) return encodeURIComponent(r[2]);
 };
+
+export const getQueryObject = (search: string): Record<string, string> => {
+  const query = search.substring(1);
+  const queryObject: Record<string, string> = {};
+  query.split("&").forEach((item: string) => {
+    const [key, value] = item.split("=");
+    queryObject[key] = value;
+  });
+  return queryObject;
+};
